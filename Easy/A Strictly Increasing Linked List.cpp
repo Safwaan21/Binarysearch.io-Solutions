c21fb8c3e1aad79b8817f2d1e6e95d0247c2ae3f
Safwaan21 <safwaanshaikh21@gmail.com>
2021-05-16 13:59:47 -0400

    Create A Strictly Increasing Linked List.cpp

---
+++ Easy/A Strictly Increasing Linked List.cpp
@@ -0,0 +1,14 @@
+/**
+ * class LLNode {
+ *     public:
+ *         int val;
+ *         LLNode *next;
+ * };
+ */
+bool solve(LLNode* head) {
+    while(head->next){
+        if(head->val >= head->next->val) return false;
+        head = head->next;
+    }
+    return true;
+}
